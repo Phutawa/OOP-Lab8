397518c41a71a08def00c7ada4610f9d85183856
Phutawa <phutawanmuangma@gmail.com>
2025-02-06 15:36:26 +0700

    Add files via upload

---
+++ Checkpoint 2/TestInterface.java
@@ -0,0 +1,12 @@
+public class TestInterface {
+    public static void main(String[] args) {
+        GeometricObject[] objects = {new Square(2), new Square(3), new Square(4.5), new Square(5), new Square(6)};
+
+        for (int i = 0; i < objects.length; i++) {
+            System.out.println("Area is: " + objects[i].getArea());
+            if (objects[i] instanceof Colorable) {
+            ((Colorable) objects[i]).howToColor();
+            }
+        }
+    }
+}
